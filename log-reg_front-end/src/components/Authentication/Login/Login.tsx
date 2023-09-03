177daf750a2e6d12df5e8515678de5f82b27993f
karoldawidg <karoldawidg@gmail.com>
2023-09-03 10:25:22 +0200

    Log 33: <frontend> captcha implementation

--- log-reg_front-end/src/components/Authentication/Login/Login.tsx
+++ log-reg_front-end/src/components/Authentication/Login/Login.tsx
@@ -1,124 +1,104 @@
-import React, { createContext, useState, useEffect} from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { createContext, useState, useEffect, useRef } from "react";
+import { ReCAPTCHA } from "react-google-recaptcha";
 import { notify } from "../../Others/Notify";
 import axios from "axios";
-import {ENDPOINT_AUTH, INTERNET_DISCONNECTED, ADMIN_ROLE, ENDPOINT_REFRESH} from "../../Utils/links";
+import { ENDPOINT_AUTH, INTERNET_DISCONNECTED, ADMIN_ROLE, ENDPOINT_REFRESH } from "../../Utils/links";
 import { LoginForm } from "./LoginForm";
 import { RedirectBtn } from "../../Others/RedirectBtn";
-import {LoginContextType} from '../../Utils/Interfaces/LoginContextType';
+import { LoginContextType } from '../../Utils/Interfaces/LoginContextType';
 import "../../../css/styles.css";
 import { Title } from "../../Others/Title";
-import jwtDecode from "jwt-decode";
 
-export const LoginContext  = createContext<LoginContextType  | null>(null); 
+export const LoginContext = createContext<LoginContextType | null>(null);
+export const CaptchaContext = createContext<React.MutableRefObject<ReCAPTCHA | null> | null>(null);
 
 export const Login = () => {
+    const captchaRef = useRef<ReCAPTCHA | null>(null);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const redirect = useNavigate();
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-        try {
-          const response = await axios.post(ENDPOINT_AUTH, {
-            username,
-            password,
-          });
+        event.preventDefault();
 
-          if (response && response.status === 200) {
-                const token = response.data.token;
-                const refreshToken = response.data.refreshToken;
-                
-                localStorage.setItem('token', token);   
-                localStorage.setItem('refreshToken', refreshToken);
-
-                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-                notify(response.data.message);
-                setIsAuthenticated(true);
-
-                const decodedToken:any = jwtDecode(token);
-                const userRole = decodedToken.role;
-
-            if (userRole === ADMIN_ROLE) {  
-                redirect('/admin');
-            } else {
-                redirect('/after-login');
-            }
-
-          } 
-        } catch (error: any) {
-          if (error) {
-            console.error(error);
-            notify(INTERNET_DISCONNECTED);
-          }
+        if (captchaRef.current) {
+            const token = captchaRef.current.getValue();
+            console.log(`token capta= ${token}`)
+            captchaRef.current.reset();
+        } else {
+            console.warn("captchaRef.current is null");
         }
     };
-    
+
+    // Obs?uga b??du sieciowego
+    const handleNetworkError = (error: any) => {
+        console.error(error);
+
+        if (error.response) {
+            notify(error.response.data);
+        } else {
+            notify(INTERNET_DISCONNECTED);
+        }
+    };
 
     const handleTokenRefresh = async () => {
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (refreshToken) {
+            try {
+                const response = await axios.post(ENDPOINT_REFRESH, {
+                    refreshToken,
+                });
 
-      const refreshToken = localStorage.getItem('refreshToken');
-      if (refreshToken) {
-        try {
-          const response = await axios.post(ENDPOINT_REFRESH, {
-            refreshToken,
-          });
-    
-          if (response.status === 200) {
-            const token = response.data.token;
-            const refreshToken = response.data.refreshToken;
-            localStorage.setItem('token', token);
-            localStorage.setItem('refreshToken', refreshToken);
-            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-            setIsAuthenticated(true);
-            notify("Token refreshed successfully.");
-          } else {
-            notify("Failed to refresh token.");
-          }
-        } catch (error: any) {
-            if (error.response) {
-              console.error(error.response.data);
-              notify(error.response.data);
-            } else {
-              console.error(error);
-              notify(INTERNET_DISCONNECTED);
+                if (response.status === 200) {
+                    const token = response.data.token;
+                    const refreshToken = response.data.refreshToken;
+                    localStorage.setItem('token', token);
+                    localStorage.setItem('refreshToken', refreshToken);
+                    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                    setIsAuthenticated(true);
+                    notify("Token refreshed successfully.");
+                } else {
+                    notify("Failed to refresh token.");
+                }
+            } catch (error: any) {
+                // Obs?uga b??du sieciowego
+                handleNetworkError(error);
             }
-          }
-      }
+        }
     };
-  
-    // Check if token exists and if not, refresh it
+
     useEffect(() => {
-      const token = localStorage.getItem('token');
-      if (!token && isAuthenticated) {
-        handleTokenRefresh();
-      }
+        const token = localStorage.getItem('token');
+        if (!token && isAuthenticated) {
+            handleTokenRefresh();
+        }
     }, [isAuthenticated]);
 
-    /////////////////////////////////////////////////////
-  return (
-  <>
-    <LoginContext.Provider 
-        value={{
-          username,
-          password,
-          setPassword,
-          setUsername,
-          handleSubmit
-        }}>
-      <Title props={'Login panel'}/>
-        <div className="container">
-          <LoginForm/>
-            <div className="left-side">
-              <div className="regist__buttons">
-                <RedirectBtn to="/">Menu</RedirectBtn>
-                <RedirectBtn to="/regist">Regist</RedirectBtn>
-                <RedirectBtn to="/reset-email">Reset</RedirectBtn>
-              </div>
-            </div>
-      </div>
-    </LoginContext.Provider>
-  </>
-  );
+    return (
+        <>
+            <LoginContext.Provider
+                value={{
+                    username,
+                    password,
+                    setPassword,
+                    setUsername,
+                    handleSubmit,
+                }}
+            >
+                <CaptchaContext.Provider value={captchaRef}>
+                    <Title props={'Login panel'} />
+                    <div className="container">
+                        <LoginForm />
+                        <div className="left-side">
+                            <div className="regist__buttons">
+                                <RedirectBtn to="/">Menu</RedirectBtn>
+                                <RedirectBtn to="/regist">Regist</RedirectBtn>
+                                <RedirectBtn to="/reset-email">Reset</RedirectBtn>
+                            </div>
+                        </div>
+                    </div>
+                </CaptchaContext.Provider>
+            </LoginContext.Provider>
+        </>
+    );
 };
--- log-reg_front-end/src/components/Authentication/Login/LoginForm.tsx
+++ log-reg_front-end/src/components/Authentication/Login/LoginForm.tsx
@@ -1,54 +1,59 @@
-import { useContext } from "react";
-import {backgroundColor, preventSpace} from "../../Utils/FormsUtils/forms-utils";
-import {LoginContext} from './Login';
+import React, { useContext } from "react";
+import { backgroundColor, preventSpace } from "../../Utils/FormsUtils/forms-utils";
+import { CaptchaContext, LoginContext } from './Login';
+import ReCAPTCHA from "react-google-recaptcha";
 import "../../../css/styles.css";
 
+// const REACT_APP_SECRET_KEY = "6Lc-ufQnAAAAAFHAfXXa-u2F7gMpSaBHRT323NgS";
+const REACT_APP_SITE_KEY = "6Lc-ufQnAAAAAACcAYt0QXQIBxulSTBdUUcAXQE-";
+
 export const LoginForm = () => {
-  
   const context = useContext(LoginContext);
-    if(!context) return null;
-      const {handleSubmit, username, password, setPassword, setUsername} = context;
+  const contextCapta = useContext(CaptchaContext);
+
+  if (!context || !contextCapta) return null;
+
+  const { handleSubmit, username, password, setPassword, setUsername } = context;
+  const captchaRef = contextCapta as React.MutableRefObject<ReCAPTCHA | null>;
 
   return (
-    <>
-    <div className="right-side">
-      <form className="login-form__form" onSubmit={handleSubmit}>
-        <label className="login-form__label" htmlFor="username">
-          Username:
-        </label>
-        <input
-          className="login-form__input"
-          minLength={4}
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          style={{backgroundColor: `${backgroundColor(username.length, 4)}`}}
-          onKeyDown={preventSpace}
-          required
-        />
-        <br />
+      <div className="right-side">
+        <form className="login-form__form" onSubmit={handleSubmit}>
+          <label className="login-form__label" htmlFor="username">
+            Username:
+          </label>
+          <input
+              className="login-form__input"
+              minLength={4}
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              style={{ backgroundColor: `${backgroundColor(username.length, 4)}` }}
+              onKeyDown={preventSpace}
+              required
+          />
+          <br />
 
-        <label className="login-form__label" htmlFor="password">
-          Password:
-        </label>
-        <input
-          className="login-form__input"
-          minLength={8}
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{backgroundColor: `${backgroundColor(password.length, 8)}`}}
-          onKeyDown={preventSpace}
-          required
-        />
-        <br />
+          <label className="login-form__label" htmlFor="password">
+            Password:
+          </label>
+          <input
+              className="login-form__input"
+              minLength={8}
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ backgroundColor: `${backgroundColor(password.length, 8)}` }}
+              onKeyDown={preventSpace}
+              required
+          />
+          <br />
+          <ReCAPTCHA sitekey={REACT_APP_SITE_KEY} ref={captchaRef} />
 
-        <input className="login-form__submit" type="submit" value="Login" />
-        
-      </form>
+          <input className="login-form__submit" type="submit" value="Login" />
+        </form>
       </div>
-    </>
   );
 };
