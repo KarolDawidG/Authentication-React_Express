0032ceba1a016e7e83e1f454e1dedfdc87ead834
KarolDawidG <karoldawidg@gmail.com>
2023-09-04 22:37:28 +0200

    Log 37: <frontend> Logi refactor

--- log-reg_front-end/src/components/Authentication/Login/Login.tsx
+++ log-reg_front-end/src/components/Authentication/Login/Login.tsx
@@ -2,15 +2,15 @@ import React, { createContext, useState, useEffect, useRef} from "react";
 import { useNavigate } from 'react-router-dom';
 import { notify } from "../../Others/Notify";
 import axios from "axios";
-import {ENDPOINT_AUTH, INTERNET_DISCONNECTED, ADMIN_ROLE, ENDPOINT_REFRESH, ENDPOINT_CAPTCHA} from "../../Utils/links";
+import {  ENDPOINT_CAPTCHA} from "../../Utils/links";
 import { LoginForm } from "./LoginForm";
 import { RedirectBtn } from "../../Others/RedirectBtn";
 import {LoginContextType} from '../../Utils/Interfaces/LoginContextType';
 import "../../../css/styles.css";
 import { Title } from "../../Others/Title";
-import jwtDecode from "jwt-decode";
 import { ReCAPTCHA } from "react-google-recaptcha";
-
+import {handleLogin} from "./handlers/loginFunctions";
+import { handleTokenRefresh } from "./handlers/tokenRefreshFunctions";
 
 export const LoginContext = createContext<LoginContextType | null>(null);
 export const CaptchaContext = createContext<React.MutableRefObject<ReCAPTCHA | null> | null>(null);
@@ -24,91 +24,29 @@ export const Login = () => {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-    
         if (captchaRef.current) {
             const inputElement = event.currentTarget[0] as HTMLInputElement; 
             const inputVal = inputElement.value;
             const token = captchaRef.current.getValue();
             captchaRef.current.reset();
 
-            try { 
-                const response = await axios.post(ENDPOINT_CAPTCHA, { inputVal, token });
-        
-                if (response.data === "Human 👨 👩") {
-                
-                    const response = await axios.post(ENDPOINT_AUTH, {
-                        username,
-                        password,
-                      });
-                    
-                      if (response && response.status === 200) {
-                        const token = response.data.token;
-                        const refreshToken = response.data.refreshToken;
-                        
-                        localStorage.setItem('token', token);   
-                        localStorage.setItem('refreshToken', refreshToken);
-        
-                        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-                        notify(response.data.message);
-                        setIsAuthenticated(true);
-        
-                        const decodedToken:any = jwtDecode(token);
-                        const userRole = decodedToken.role;
-        
-                    if (userRole === ADMIN_ROLE) {  
-                        redirect('/admin');
-                    } else {
-                        redirect('/after-login');
-                    }
-                  }    
+                try {
+                    const response = await axios.post(ENDPOINT_CAPTCHA, { inputVal, token });
+
+                        if (response.data === "Human 👨 👩") {
+                            handleLogin(username, password, setIsAuthenticated, redirect);
+                        }
+                } catch (error) {
+                    console.error(error);
+                    notify("An error occurred while logging in.");
                 }
-            } catch (error) {
-                console.error(error);
-                notify("Wystąpił błąd podczas logowaniaaa.");
-            }
         }
     };
-    
-
-    const handleNetworkError = (error: any) => {
-        console.error(error);
-
-        if (error.response) {
-            notify(error.response.data);
-        } else {
-            notify(INTERNET_DISCONNECTED);
-        }
-    };
-
-    const handleTokenRefresh = async () => {
-        const refreshToken = localStorage.getItem('refreshToken');
-        if (refreshToken) {
-            try {
-                const response = await axios.post(ENDPOINT_REFRESH, {
-                    refreshToken,
-                });
-
-                if (response.status === 200) {
-                    const token = response.data.token;
-                    const refreshToken = response.data.refreshToken;
-                    localStorage.setItem('token', token);
-                    localStorage.setItem('refreshToken', refreshToken);
-                    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-                    setIsAuthenticated(true);
-                    notify("Token refreshed successfully.");
-                } else {
-                    notify("Failed to refresh token.");
-                }
-            } catch (error: any) {
-                handleNetworkError(error);
-            }
-        }
-    };
-
+  
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token && isAuthenticated) {
-            handleTokenRefresh();
+            handleTokenRefresh(setIsAuthenticated);
         }
     }, [isAuthenticated]);
 
