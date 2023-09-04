07a478056c3c7e33b77e6166e737b1ba18c8f09b
KarolDawidG <karoldawidg@gmail.com>
2023-09-04 16:50:38 +0200

    Log 34: <frontend> react-spinners problem

--- log-reg_front-end/src/components/Utils/Loader.tsx
+++ log-reg_front-end/src/components/Utils/Loader.tsx
@@ -1,17 +1,28 @@
-import {FadeLoader} from "react-spinners/";
 
 export const Loader = () => {
   return <>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <FadeLoader
-            color="#e30937"
-            height={30}
-            loading
-            margin={6}
-            radius={3}
-            speedMultiplier={1}
-            width={4}
-        />
+    <div>
+      <h1>Loading...</h1>
     </div>;
   </>
 }
+// There was an issue with react-spinners, so I decided to comment out those lines of code.
+
+
+// import {FadeLoader} from "react-spinners/";
+
+// export const Loader = () => {
+//   return <>
+//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+//         <FadeLoader
+//             color="#e30937"
+//             height={30}
+//             loading
+//             margin={6}
+//             radius={3}
+//             speedMultiplier={1}
+//             width={4}
+//         />
+//     </div>;
+//   </>
+// }
