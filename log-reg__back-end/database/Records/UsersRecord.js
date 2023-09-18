381ebe2b8486fae45b0fd04d63c8b0bf842b1f5a
KarolDawidG <karoldawidg@gmail.com>
2023-09-18 14:56:02 +0200

    Log 56: <backend> querryUsersRecord added

--- log-reg__back-end/database/Records/UsersRecord.js
+++ log-reg__back-end/database/Records/UsersRecord.js
@@ -1,8 +1,7 @@
 const {pool} = require("../db");
 const { validateEmail, validateUserName } = require('../../config/config');
 const { v4: uuidv4 } = require('uuid');
-const { USER_EXIST } = require("../../config/messages");
-
+const { INSERT, ACTIVE, DELETE, UPDATE_BY_ID, UPDATE_ROLE, SELECT_ALL, SELECT_BY_EMAIL, SELECT_BY_ID, SELECT_BY_USERNAME } = require("./querryUsersRecord");
 
 
 class UsersRecord{
@@ -30,81 +29,71 @@ class UsersRecord{
         }
       }
 
+    static async insert(username, hashPassword, email){
+        if (!validateEmail(email)) {
+          throw new Error('Invalid email address.');
+        }
+    
+        if (!validateUserName(username)) {
+          throw new Error('Invalid username.');
+        }
+        const id = uuidv4();
+  
+          return UsersRecord.performTransaction(async (connection) => {
+            await connection.execute(INSERT, [id, username, hashPassword, email]);
+              return id;
+          })
+      }
+
     static async activateAccount(id) {
       return UsersRecord.performTransaction(async (connection) => {
-        const results = await connection.execute("UPDATE accounts SET is_active = true WHERE id = ?", [id]);
+        const results = await connection.execute(ACTIVE, [id]);
           return results;
       });
     }
 
-
     static async delete(id) {
       return UsersRecord.performTransaction(async (connection) => {
-       const result = await connection.execute("DELETE FROM accounts WHERE id = ?", [id]);
+       const result = await connection.execute(DELETE, [id]);
           return result;
       });
     }
 
-    static async insert(username, hashPassword, email){
-      if (!validateEmail(email)) {
-        throw new Error('Invalid email address.');
-      }
-  
-      if (!validateUserName(username)) {
-        throw new Error('Invalid username.');
-      }
-      const id = uuidv4();
-
-        return UsersRecord.performTransaction(async (connection) => {
-          await connection.execute("INSERT INTO accounts (id, username, password, email) VALUES (?, ?, ?, ?)", [id, username, hashPassword, email]);
-            return id;
-        })
-    }
-
-    static async updatePasswordByEmail([ hashPassword, email]) {
-        return UsersRecord.performTransaction(async (connection) => {
-          const results = await connection.execute("UPDATE accounts SET password = ? WHERE email = ?", [hashPassword, email]);
-            return results;
-        });
-    }
-
     static async updatePasswordById([ hashPassword, id]) {
       return UsersRecord.performTransaction(async (connection) => {
-        const results = await connection.execute("UPDATE accounts SET password = ? WHERE id = ?", [hashPassword, id]);
+        const results = await connection.execute(UPDATE_BY_ID, [hashPassword, id]);
           return results;
       });
     }
     
     static async updateRole(role, username) {
       return UsersRecord.performTransaction(async(connection) => {
-        const results = await connection.execute('UPDATE accounts SET role = ? WHERE username = ?', [role, username]);
+        const results = await connection.execute(UPDATE_ROLE, [role, username]);
           return results;
       })
     }
-    ////////////////////////////////////////////////////////////////////////////////
 
     static async listAll(){
-      const [results] = await pool.execute(`SELECT * FROM accounts`);
+      const [results] = await pool.execute(SELECT_ALL);
       return results.map(obj => new UsersRecord(obj));
   }
 
   static async selectByEmail(email) {
-    const [results] = await pool.execute('SELECT * FROM accounts WHERE email = ?',email);
+    const [results] = await pool.execute(SELECT_BY_EMAIL,email);
     return results; 
   }
   
-
   static async selectById(id){
-    const [results] = await pool.execute('SELECT * FROM accounts WHERE id = ?',id);
+    const [results] = await pool.execute(SELECT_BY_ID,id);
     return results;
   }
 
   static async selectByUsername(username){
-    const [results] = await pool.execute('SELECT is_active, role, password FROM accounts WHERE username = ?', username);
+    const [results] = await pool.execute(SELECT_BY_USERNAME, username);
     return results;
   }
 
-  }
+}
         
 module.exports = {
     UsersRecord,
