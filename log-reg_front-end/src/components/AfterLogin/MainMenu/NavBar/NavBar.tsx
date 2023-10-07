2141000de7f58a79cf33a2dd0c65510ff6eb507f
KarolDawidG <karoldawidg@gmail.com>
2023-10-07 14:24:57 +0200

    Log 100:<frontend> new NavBar

--- log-reg_front-end/src/components/AfterLogin/MainMenu/NavBar/NavBar.tsx
+++ log-reg_front-end/src/components/AfterLogin/MainMenu/NavBar/NavBar.tsx
@@ -1,24 +1,28 @@
-import { LogoutButton } from "../../../Others/LogoutButton"
+import { LogoutButton } from "../../../Others/LogoutButton";
 import { RedirectBtn } from "../../../Others/RedirectBtn";
 import "./NavBar.css";
 
-export const NavBar = () =>{
+export const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className="checkbtn">
+        <i className="fas fa-bars">|||</i>
+      </label>
 
-return(
-<>
-  <div className="nav">
-    <div className="nav_menu">
-      <nav>
-        <ul>
-          <li><RedirectBtn to={`https://react-g-rock-paper-scissors.netlify.app/contact`}>
-              Contact</RedirectBtn></li>
-          <li><RedirectBtn to={`https://github.com/KarolDawidG`}>
-              About</RedirectBtn></li>
-          <li><LogoutButton /></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-</>
-    )
-}
+      <label className="logo">Super Testy</label>
+          <ul>
+              <li>
+                <a href="https://github.com/KarolDawidG">About</a>
+              </li>
+              <li>
+                <a href="https://react-g-rock-paper-scissors.netlify.app/contact">Contact</a>
+              </li>
+              <li>
+                <LogoutButton />
+              </li>
+          </ul>
+    </nav>
+  );
+};
+
